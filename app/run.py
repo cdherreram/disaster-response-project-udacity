b81ecad55661f3d446e536a4cb7996cf29353fda
David Herrera <cristian.herrera@davivienda.com>
2022-02-10 18:29:46 -0500

    Changes in import heading of run.py

--- app/run.py
+++ app/run.py
@@ -8,7 +8,7 @@ from nltk.tokenize import word_tokenize
 from flask import Flask
 from flask import render_template, request, jsonify
 from plotly.graph_objs import Bar
-from sklearn.externals import joblib
+import joblib
 from sqlalchemy import create_engine
 
 
@@ -31,7 +31,6 @@ df = pd.read_sql_table('MessageCategories', engine)
 
 # load model
 model = joblib.load("../models/classifier.pkl")
-
 
 # index webpage displays cool visuals and receives user input text for model
 @app.route('/')
